4e0e2f561e66e4c692f453ae044a73f4ede2407e
Boris <boris.dorofeev@gmail.com>
2017-01-23 16:04:43 +0600

    Добавлены функций коллекций сумма, минимум, максимум

--- src/Collections/GroupCollection.php
+++ src/Collections/GroupCollection.php
@@ -53,6 +53,97 @@ class GroupCollection extends NamedCollection implements GroupCollectionInterfac
         parent::addByName($groupItem);
     }
 
+    private function funcByField($field_name, $func_name)
+    {
+        if(!$this->groupType->ownExist($field_name))
+        {
+            throw new ExtractorException('Поле для получения '.$func_name.' '.$field_name.' не найдено в собственных полях типа '.$this->getName().'!');
+        }
+
+        //НEоптимизированный метод пока! переделать при случае
+
+        if($func_name === 'max' or $func_name === 'min')
+        {
+            $currentItem = null;
+
+            $current_value = null;
+
+            foreach($this as $item)
+            {
+                if($func_name === 'max')
+                {
+                    $ok = $item->$field_name > $current_value;
+                }
+                else
+                {
+                    $ok = $item->$field_name < $current_value;
+                }
+
+                if($ok or $current_value === null)
+                {
+                    $current_value = $item->$field_name;
+                    $currentItem = $item;
+                }
+            }
+
+            return $currentItem;
+        }
+        elseif($func_name === 'sum')
+        {
+            $sum = 0;
+
+            foreach($this as $item)
+            {
+                $value = $item->$field_name;
+
+                if(is_numeric($value))
+                {
+                    $sum += $value;
+                }
+                else
+                {
+                    return $sum;
+                }
+            }
+
+            return $sum;
+        }
+        else
+        {
+            throw new ExtractorException('Функция '.$func_name.' не поддерживается в коллекции элементов групп!');
+        }
+    }
+
+    /**
+     * @param $field_name
+     *
+     * @return \Interpro\Extractor\Contracts\Items\GroupItem
+     */
+    public function maxByField($field_name)
+    {
+        return $this->funcByField($field_name, 'max');
+    }
+
+    /**
+     * @param $field_name
+     *
+     * @return \Interpro\Extractor\Contracts\Items\GroupItem
+     */
+    public function minByField($field_name)
+    {
+        return $this->funcByField($field_name, 'min');
+    }
+
+    /**
+     * @param $field_name
+     *
+     * @return int
+     */
+    public function sumByField($field_name)
+    {
+        return $this->funcByField($field_name, 'sum');
+    }
+
     /**
      * @return string
      */
--- src/Contracts/Collections/GroupCollection.php
+++ src/Contracts/Collections/GroupCollection.php
@@ -26,4 +26,25 @@ interface GroupCollection extends Collection, Named
      * @return void
      */
     public function addItem(GroupItem $groupItem);
+
+    /**
+     * @param $field_name
+     *
+     * @return \Interpro\Extractor\Contracts\Items\GroupItem
+     */
+    public function maxByField($field_name);
+
+    /**
+     * @param $field_name
+     *
+     * @return \Interpro\Extractor\Contracts\Items\GroupItem
+     */
+    public function minByField($field_name);
+
+    /**
+     * @param $field_name
+     *
+     * @return int
+     */
+    public function sumByField($field_name);
 }
